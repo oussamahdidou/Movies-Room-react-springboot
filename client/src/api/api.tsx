66dcc5a25fdf8c998a491d70666e3c9e0f1838e8
oussamahdidou <oussamahdidou15@gmail.com>
2024-09-02 02:37:29 +0100

    list of movies

--- client/src/api/api.tsx
+++ client/src/api/api.tsx
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import { Movie } from '../Interfaces/Movie';
 
-const apiKey = process.env.REACT_APP_API_KEY; // Make sure your .env file is correctly set up
+const apiKey = process.env.REACT_APP_API_KEY; 
 
 export const TodayTrends = async (): Promise<Movie[]> => {
   try {
-    console.log('API Key:', apiKey); // Log the API Key
+    console.log('API Key:', apiKey); 
     const response = await axios.get<any>(`https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}&page=1`);
     console.log('API Response:', response.data);
     return response.data.results;
   } catch (error) {
     console.error('Error in TodayTrends:', error);
-    return []; // Return an empty array on error
+    return []; 
   }
 }
