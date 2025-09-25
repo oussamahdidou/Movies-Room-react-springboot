d6622cb8a138f182508c161968b2287a146cbc48
oussama hdidou <oussamahdidou15@gmail.com>
2025-09-25 12:40:50 +0100

    Update api.tsx
    delete adult movies

--- client/src/api/api.tsx
+++ client/src/api/api.tsx
@@ -5,7 +5,7 @@ const apiKey = process.env.REACT_APP_API_KEY;
 
 export const TodayTrends = async (Periode:string): Promise<Movie[]> => {
   try {
-    const response = await axios.get<any>(`https://api.themoviedb.org/3/trending/all/${Periode}?api_key=${apiKey}&page=1&include_adult=true`);
+    const response = await axios.get<any>(`https://api.themoviedb.org/3/trending/all/${Periode}?api_key=${apiKey}&page=1&include_adult=false`);
     return response.data.results;
   } catch (error) {
     console.error('Error in TodayTrends:', error);
@@ -14,7 +14,7 @@ export const TodayTrends = async (Periode:string): Promise<Movie[]> => {
 }
 export const SearchMovies = async (Query:string):Promise<Movie[]> => {
   try {
-     const response = await axios.get<any>(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${Query}&page=1&include_adult=true`);
+     const response = await axios.get<any>(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${Query}&page=1&include_adult=false`);
     return response.data.results;
   } catch (error) {
     console.error('Error in TodayTrends:', error);
@@ -51,7 +51,7 @@ export const GetMovieTrailers = async (Id:number):Promise<MovieTrailer[]> => {
 }
 export const SimilarMovies = async (Id:number):Promise<Movie[]> => {
   try {
-     const response = await axios.get<any>(`https://api.themoviedb.org/3/movie/${Id}/similar?api_key=${apiKey}&include_adult=true`);
+     const response = await axios.get<any>(`https://api.themoviedb.org/3/movie/${Id}/similar?api_key=${apiKey}&include_adult=false`);
     return response.data.results;
   } catch (error) {
     console.error('Error in TodayTrends:', error);
@@ -60,7 +60,7 @@ export const SimilarMovies = async (Id:number):Promise<Movie[]> => {
 }
 export const MoviesRecommandations = async (Id:number):Promise<Movie[]> => {
   try {
-     const response = await axios.get<any>(`https://api.themoviedb.org/3/movie/${Id}/recommendations?api_key=${apiKey}&include_adult=true`);
+     const response = await axios.get<any>(`https://api.themoviedb.org/3/movie/${Id}/recommendations?api_key=${apiKey}&include_adult=false`);
     return response.data.results;
   } catch (error) {
     console.error('Error in TodayTrends:', error);
